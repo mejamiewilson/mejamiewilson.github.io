169f8855801dd3026b87732076d42023844e69ee
Jamie Wilson <jamie.wilsons@gmail.com>
2017-01-31 17:00:54 -0800

    adds base form

--- js/settings.js
+++ js/settings.js
@@ -3,21 +3,29 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-var fruitSelector = document.getElementById('fruit');
-var vegetableSelector = document.getElementById('vegetable');
+var addMilestoneSelector = document.getElementById('js-add-milestone');
+var milestonesElementSelector = document.getElementById('milestones');
+
+// var fruitSelector = document.getElementById('fruit');
+// var vegetableSelector = document.getElementById('vegetable');
 
 t.render(function(){
   return Promise.all([
+    t.get('card', 'private', 'milestones'),
     t.get('board', 'shared', 'fruit'),
     t.get('board', 'private', 'vegetable')
   ])
-  .spread(function(savedFruit, savedVegetable){
-    if(savedFruit && /[a-z]+/.test(savedFruit)){
-      fruitSelector.value = savedFruit;
+  .spread(function(savedMilestones, savedFruit, savedVegetable){
+    //set the values
+    if(savedMilestones) {
+      milestonesElementSelector.innerHTML = JSON.stringify(savedMilestones);
     }
-    if(savedVegetable && /[a-z]+/.test(savedVegetable)){
-      vegetableSelector.value = savedVegetable;
-    }
+    // if(savedFruit && /[a-z]+/.test(savedFruit)){
+    //   fruitSelector.value = savedFruit;
+    // }
+    // if(savedVegetable && /[a-z]+/.test(savedVegetable)){
+    //   vegetableSelector.value = savedVegetable;
+    // }
   })
   .then(function(){
     t.sizeTo('#content')
@@ -25,12 +33,22 @@ t.render(function(){
   })
 });
 
+var milestoneRenderer = function() {
+  var div = document.createElement("div");
+  div.innerHTML = "New Milestone Form";
+  return div;
+};
+
+addMilestoneSelector.addEventListener('click', function() {
+  milestonesElementSelector.appendChild(milestoneRenderer());
+});
+
 document.getElementById('save').addEventListener('click', function(){
-  return t.set('board', 'private', 'vegetable', vegetableSelector.value)
-  .then(function(){
-    return t.set('board', 'shared', 'fruit', fruitSelector.value);
-  })
-  .then(function(){
-    t.closePopup();
-  })
-})
+  // return t.set('board', 'private', 'vegetable', vegetableSelector.value)
+  // .then(function(){
+  //   return t.set('board', 'shared', 'fruit', fruitSelector.value);
+  // })
+  // .then(function(){
+    return t.closePopup();
+  //})
+});
