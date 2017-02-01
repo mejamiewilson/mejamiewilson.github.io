6e63eed680b65d4f2c2b861947b20ec1ae6bc3d3
Jamie Wilson <jamie.wilsons@gmail.com>
2017-01-31 21:19:47 -0800

    dates

--- js/board-bar.js
+++ js/board-bar.js
@@ -7,6 +7,7 @@ var milestones = [];
 var matchedData = {};
 var canvasElement = document.getElementById("result");
 var dayWidth = 0;
+var today = new Date();
 
 t.render(function(){
 
@@ -92,6 +93,28 @@ var renderBar = function(cardId, barObj) {
 
   console.log("Milestone, min max", min, max);
 
+  var daysBetweenMin = days_between(min, today);
+  var daysBetweenMax = days_between(max, today);
 
+  console.log("Days between", daysBetweenMin, daysBetweenMax);
 
 };
+
+
+
+function days_between(date1, date2) {
+
+    // The number of milliseconds in one day
+    var ONE_DAY = 1000 * 60 * 60 * 24
+
+    // Convert both dates to milliseconds
+    var date1_ms = date1.getTime()
+    var date2_ms = date2.getTime()
+
+    // Calculate the difference in milliseconds
+    var difference_ms = Math.abs(date1_ms - date2_ms)
+
+    // Convert back to days and return
+    return Math.round(difference_ms/ONE_DAY)
+
+}
