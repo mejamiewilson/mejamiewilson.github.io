2928acf1d252e69afa2b82dd1dca83878ff787c0
Jamie Wilson <jamie.wilsons@gmail.com>
2017-01-31 18:07:21 -0800

    get board cards

--- js/board-bar.js
+++ js/board-bar.js
@@ -4,10 +4,19 @@ var t = TrelloPowerUp.iframe();
 
 t.render(function(){
 
+  
+
   return t.cards('id', 'name')
     .then(function(promiseResult) {
       console.log(promiseResult);
     })
+    .then(function() {
+      return t.get('card', 'public', 'milestones');
+    })
+    .then(function(results) {
+      console.log("get milestones from cards results", results);
+    });
+  
   // this function we be called once on initial load
   // and then called each time something changes that
   // you might want to react to, such as new data being
